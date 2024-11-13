daa5e5cfec0d3ae2cf0e134953dfb71890c98092
marcocimarius <mmarcoci55@gmail.com>
2024-11-13 11:28:39 +0100

    efc

---
+++ Server/EfcRepository/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfcRepository;
+
+public class AppContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = app.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<EfcUser>().HasKey(x => x.Id);
+        modelBuilder.Entity<EfcComment>().HasKey(x => x.Id);
+        modelBuilder.Entity<EfcPost>().HasKey(x => x.Id);
+    }
+}
